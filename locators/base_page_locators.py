f9f293ed8e2b23ffca148f68f7561e81c7170b02
lenlen1rem <lissious@yandex.ru>
2026-02-06 17:10:32 +0700

    Create base_page_locators.py

---
+++ locators/base_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class BasePageLocators:
+
+    button_order_header_locator = By.CLASS_NAME, "Button_Button__ra12g"                    #кнопка "Заказать" в заголовке страницы
+    button_status_order_locator = By.CLASS_NAME, "Header_Link__1TAG7"                      #кнопка "Статус заказа" в заголовке страницы
+    button_scooter_locator = By.XPATH, '//a[@class="Header_LogoScooter__3lsAR"]'           #логотип "Самокат" в заголовке страницы
+    button_yandex_locator = By.XPATH, '//a[@class ="Header_LogoYandex__3TSOI"]'            #логотип "Яндекс" в заголовке страницы
+    button_accept_cookie_locator = By.ID, "rcc-confirm-button"                             #кнопка принятия куки - "да все привыкли"
