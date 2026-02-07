9aac1e1af5d2cd218610933d88ff37064a98126e
lenlen1rem <lissious@yandex.ru>
2026-02-07 16:32:01 +0700

    Update test_main_page.py

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,15 +1,19 @@
 import pytest
 import allure
 import data
+from data import DataFAQ
 from pages.main_page import MainPage
+
 
 
 class TestMainPage:
     @allure.title('Проверка вопросов и ответов')
     @allure.description('Кликаем на вопрос, получаем ответ, сравниваем с ожидаемым ответом')
-    @pytest.mark.parametrize( 'question_num' , [ 0, 1, 2, 3, 4, 5, 6, 7 ] )
-    def test_answer_for_question(self, main_page, question_num):
+    @pytest.mark.parametrize("number, question, expected_answer", DataFAQ.ANSWERS)
+    def test_answer_for_question(self, driver, number, question, expected_answer):
+        main_page = MainPage(driver)
         main_page.close_cookies()
-        main_page.click_to_question(question_num)
-        answer_text = main_page.get_answer_text(question_num)
-        assert answer_text == data.ANSWERS[question_num]
+        main_page.click_on_faq(number)
+
+        actual_answer = main_page.get_faq_text_answers(number)
+        assert actual_answer == expected_answer
